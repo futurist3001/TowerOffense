b253d6669888b92722913af4dea0dfe97f55fd7c
Vadym Karas <vadimkaras19@gmail.com>
2024-03-22 12:00:06 +0200

    Projectile_v.0.0.3

--- Source/TowerOffense/Projectile.cpp
+++ Source/TowerOffense/Projectile.cpp
@@ -1,15 +1,31 @@
 #include "Projectile.h"
 
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 AProjectile::AProjectile(const FObjectInitializer& ObjectInitializer)
 : Super(ObjectInitializer)
 {
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
-
 	ProjectileMovementComponent =
 		CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
 
 	ProjectileMovementComponent->InitialSpeed = 1000.f;
 	ProjectileMovementComponent->MaxSpeed = 3000.f;
+}
+
+void AProjectile::BeginPlay()
+{
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+}
+
+void AProjectile::OnHit(
+	UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	UKismetSystemLibrary::PrintString(this, "Hit", true, false, FColor::Red, 2.f);
+
+	//this->Destroy(); // doesnt work, i dont understand the reason
+
+	HitComponent->DestroyComponent(); // here is only for mesh component
 }
--- Source/TowerOffense/Projectile.h
+++ Source/TowerOffense/Projectile.h
@@ -20,4 +20,13 @@ public:
 
 public:	
 	AProjectile(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void OnHit(
+		UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		FVector NormalImpulse, const FHitResult& Hit);
 };
